e230f23af18a41aba5436624a2dc139c264a024f
bistukk <2751267327@qq.com>
2023-02-26 22:53:51 +0800

    栈和队列练习

--- stack and queue exercises/stack and queue exercises/stack and queue exercises.c
+++ stack and queue exercises/stack and queue exercises/stack and queue exercises.c
@@ -120,6 +120,9 @@
 //	return ret;
 //}
 
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
 
 
 //leetcode 225. 用队列实现栈
@@ -233,8 +236,9 @@
 //	assert(pq);
 //	return pq->head == NULL;
 //}
-//
-//
+
+//******************************************************************************
+
 //typedef struct {
 //	Queue q1;
 //	Queue q2;
@@ -311,4 +315,157 @@
 // * bool param_4 = myStackEmpty(obj);
 //
 // * myStackFree(obj);
+//*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+//typedef int STDataType;
+//typedef struct Stack
+//{
+//	STDataType* a;
+//	int top;
+//	int capacity;
+//}ST;
+//
+//void StackInit(ST* ps);
+//void StackDestroy(ST* ps);
+//void StackPush(ST* ps, STDataType x);
+//void StackPop(ST* ps);
+//STDataType StackTop(ST* ps);
+//int StackSize(ST* ps);
+//bool StackEmpty(ST* ps);
+//
+//void StackInit(ST* ps)
+//{
+//	assert(ps);
+//	ps->a = NULL;
+//	ps->top = 0;
+//	ps->capacity = 0;
+//}
+//
+//void StackPush(ST* ps, STDataType x)
+//{
+//	assert(ps);
+//	if (ps->top == ps->capacity)
+//	{
+//		int newCapacity = ps->capacity == 0 ? 4 : ps->capacity * 2;
+//		STDataType* tmp = realloc(ps->a, sizeof(STDataType) * newCapacity);
+//		if (tmp == NULL)
+//		{
+//			printf("realloc fail\n");
+//			exit(-1);
+//		}
+//		ps->a = tmp;
+//		ps->capacity = newCapacity;
+//	}
+//	ps->a[ps->top] = x;
+//	ps->top++;
+//}
+//
+//void StackPop(ST* ps)
+//{
+//	assert(ps);
+//	assert(ps->top > 0);
+//	ps->top--;
+//}
+//
+//STDataType StackTop(ST* ps)
+//{
+//	assert(ps);
+//	assert(!StackEmpty(ps));
+//	{
+//		return ps->a[ps->top - 1];
+//	}
+//}
+//
+//int StackSize(ST* ps)
+//{
+//	assert(ps);
+//	return ps->top;
+//}
+//
+//bool StackEmpty(ST* ps)
+//{
+//	assert(ps);
+//	return ps->top == 0;
+//}
+//
+//void StackDestroy(ST* ps)
+//{
+//	assert(ps);
+//	free(ps->a);
+//	ps->a = NULL;
+//	ps->capacity = ps->top = 0;
+//}
+//
+////*********************************************************
+//
+//typedef struct {
+//	ST PushST;
+//	ST PopST;
+//} MyQueue;
+//
+//
+//MyQueue* myQueueCreate() {
+//	MyQueue* q = (MyQueue*)malloc(sizeof(MyQueue));
+//	StackInit(&q->PushST);
+//	StackInit(&q->PopST);
+//	return q;
+//}
+//
+//void myQueuePush(MyQueue* obj, int x) {
+//	StackPush(&obj->PushST, x);
+//}
+//
+//int myQueuePop(MyQueue* obj) {
+//	//如果PopST中没有数据，将PushST中的数据导过去，使得PopST的数据符合先进先出；
+//	if (StackEmpty(&obj->PopST))
+//	{
+//		while (!StackEmpty(&obj->PushST))
+//		{
+//			StackPush(&obj->PopST, StackTop(&obj->PushST));
+//			StackPop(&obj->PushST);
+//		}
+//	}
+//	int front = StackTop(&obj->PopST);
+//	StackPop(&obj->PopST);
+//	return front;
+//}
+//
+//int myQueuePeek(MyQueue* obj) {
+//	if (StackEmpty(&obj->PopST))
+//	{
+//		while (!StackEmpty(&obj->PushST))
+//		{
+//			StackPush(&obj->PopST, StackTop(&obj->PushST));
+//			StackPop(&obj->PushST);
+//		}
+//	}
+//	return StackTop(&obj->PopST);
+//}
+//
+//bool myQueueEmpty(MyQueue* obj) {
+//	return StackEmpty(&obj->PopST) && StackEmpty(&obj->PushST);
+//}
+//
+//void myQueueFree(MyQueue* obj) {
+//	StackDestroy(&obj->PushST);
+//	StackDestroy(&obj->PopST);
+//	free(obj);
+//}
+//
+///**
+// * Your MyQueue struct will be instantiated and called as such:
+// * MyQueue* obj = myQueueCreate();
+// * myQueuePush(obj, x);
+//
+// * int param_2 = myQueuePop(obj);
+//
+// * int param_3 = myQueuePeek(obj);
+//
+// * bool param_4 = myQueueEmpty(obj);
+//
+// * myQueueFree(obj);
 //*/
