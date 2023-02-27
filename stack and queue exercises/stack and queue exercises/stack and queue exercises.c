7a9920eb2841185bf96738a8c1c4ea43c83b0c78
bistukk <2751267327@qq.com>
2023-02-27 19:20:23 +0800

    栈和队列练习

--- stack and queue exercises/stack and queue exercises/stack and queue exercises.c
+++ stack and queue exercises/stack and queue exercises/stack and queue exercises.c
@@ -470,3 +470,104 @@
 //
 // * myQueueFree(obj);
 //*/
+
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+//leetcode 622. 设计循环队列
+//typedef struct {
+//    int* a;
+//    int front;
+//    int tail;
+//    int k;
+//} MyCircularQueue;
+//
+//bool myCircularQueueIsEmpty(MyCircularQueue* obj);
+//bool myCircularQueueIsFull(MyCircularQueue* obj);
+//
+//MyCircularQueue* myCircularQueueCreate(int k) {
+//    MyCircularQueue* cq = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+//    cq->a = (int*)malloc(sizeof(int) * (k + 1));
+//    cq->front = cq->tail = 0;
+//    cq->k = k;
+//    return cq;
+//}
+//
+//bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+//    //检查是否队满
+//    if (myCircularQueueIsFull(obj))
+//    {
+//        return false;
+//    }
+//    obj->a[obj->tail] = value;
+//    ++obj->tail;
+//    obj->tail %= (obj->k + 1);
+//    return true;
+//}
+//
+//bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+//    //检查是否队空
+//    if (myCircularQueueIsEmpty(obj))
+//    {
+//        return false;
+//    }
+//    ++obj->front;
+//    obj->front %= (obj->k + 1);
+//    return true;
+//}
+//
+//int myCircularQueueFront(MyCircularQueue* obj) {
+//    if (myCircularQueueIsEmpty(obj))
+//    {
+//        return -1;
+//    }
+//    return obj->a[obj->front];
+//}
+//
+//int myCircularQueueRear(MyCircularQueue* obj) {
+//    if (myCircularQueueIsEmpty(obj))
+//    {
+//        return -1;
+//    }
+//    if (obj->tail == 0)
+//    {
+//        return obj->a[obj->k];
+//    }
+//    else
+//    {
+//        return obj->a[obj->tail - 1];
+//    }
+//}
+//
+//bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+//    return obj->front == obj->tail;
+//}
+//
+//bool myCircularQueueIsFull(MyCircularQueue* obj) {
+//    return (obj->tail + 1) % (obj->k + 1) == obj->front;
+//}
+//
+//void myCircularQueueFree(MyCircularQueue* obj) {
+//    free(obj->a);
+//    free(obj);
+//}
+//
+///**
+// * Your MyCircularQueue struct will be instantiated and called as such:
+// * MyCircularQueue* obj = myCircularQueueCreate(k);
+// * bool param_1 = myCircularQueueEnQueue(obj, value);
+//
+// * bool param_2 = myCircularQueueDeQueue(obj);
+//
+// * int param_3 = myCircularQueueFront(obj);
+//
+// * int param_4 = myCircularQueueRear(obj);
+//
+// * bool param_5 = myCircularQueueIsEmpty(obj);
+//
+// * bool param_6 = myCircularQueueIsFull(obj);
+//
+// * myCircularQueueFree(obj);
+//*/
